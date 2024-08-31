9c667eec5a04692c5f0700758cc86e6b45657f7c
treanor <scyros2000@hotmail.com>
2024-08-31 15:29:12 -0500

    Added cooldown timer

--- MACDStratUnlocked.cs
+++ MACDStratUnlocked.cs
@@ -25,116 +25,154 @@ using NinjaTrader.NinjaScript.DrawingTools;
 //This namespace holds Strategies in this folder and is required. Do not change it. 
 namespace NinjaTrader.NinjaScript.Strategies
 {
-	public class MACDStratUnlocked : Strategy
-	{
-		private MACD macd;
-		private SMA sma200;
+    public class MACDStratUnlocked : Strategy
+    {
+        private MACD macd;
+        private SMA sma200;
+        private DateTime lastPositionExitTime;
+		
 
-		protected override void OnStateChange()
-		{
-			if (State == State.SetDefaults)
-			{
-				Description = @"Enter the description for your new custom Strategy here.";
-				Name = "MACDStratUnlocked";
-				Calculate = Calculate.OnBarClose;
-				EntriesPerDirection = 1;
-				EntryHandling = EntryHandling.AllEntries;
-				IsExitOnSessionCloseStrategy = true;
-				ExitOnSessionCloseSeconds = 30;
-				IsFillLimitOnTouch = false;
-				MaximumBarsLookBack = MaximumBarsLookBack.TwoHundredFiftySix;
-				OrderFillResolution = OrderFillResolution.Standard;
-				Slippage = 0;
-				StartBehavior = StartBehavior.WaitUntilFlat;
-				TimeInForce = TimeInForce.Gtc;
-				TraceOrders = false;
-				RealtimeErrorHandling = RealtimeErrorHandling.StopCancelClose;
-				StopTargetHandling = StopTargetHandling.PerEntryExecution;
-				BarsRequiredToTrade = 20;
-				IsInstantiatedOnEachOptimizationIteration = true;
+        protected override void OnStateChange()
+        {
+            if (State == State.SetDefaults)
+            {
+                Description = @"Enter the description for your new custom Strategy here.";
+                Name = "MACDStratUnlocked";
+                Calculate = Calculate.OnBarClose;
+                EntriesPerDirection = 1;
+                EntryHandling = EntryHandling.AllEntries;
+                IsExitOnSessionCloseStrategy = true;
+                ExitOnSessionCloseSeconds = 30;
+                IsFillLimitOnTouch = false;
+                MaximumBarsLookBack = MaximumBarsLookBack.TwoHundredFiftySix;
+                OrderFillResolution = OrderFillResolution.Standard;
+                Slippage = 0;
+                StartBehavior = StartBehavior.WaitUntilFlat;
+                TimeInForce = TimeInForce.Gtc;
+                TraceOrders = false;
+                RealtimeErrorHandling = RealtimeErrorHandling.StopCancelClose;
+                StopTargetHandling = StopTargetHandling.PerEntryExecution;
+                BarsRequiredToTrade = 20;
+                IsInstantiatedOnEachOptimizationIteration = true;
 
-				FastPeriod = 12;
-				SlowPeriod = 26;
-				SignalPeriod = 9;
-				SmaPeriod = 200;
-				StopLossTicks = 10; // Default stop loss of 10 ticks
-				TakeProfitTicks = 20; // Default take profit of 20 ticks
-			}
-			else if (State == State.Configure)
-			{
-				macd = MACD(FastPeriod, SlowPeriod, SignalPeriod);
-				AddChartIndicator(macd);
+                FastPeriod = 12;
+                SlowPeriod = 26;
+                SignalPeriod = 9;
+                SmaPeriod = 200;
+                StopLossTicks = 20;
+                TakeProfitTicks = 40;
+                CooldownMinutes = 10;
 
-				sma200 = SMA(SmaPeriod);
-				AddChartIndicator(sma200);
+                // Initialize lastPositionExitTime to the first bar's time
+                lastPositionExitTime = DateTime.MinValue;
+                Print("Initialized lastPositionExitTime: " + lastPositionExitTime);
+            }
+            else if (State == State.Configure)
+            {
+                macd = MACD(FastPeriod, SlowPeriod, SignalPeriod);
+                AddChartIndicator(macd);
 
-				SetStopLoss(CalculationMode.Ticks, StopLossTicks);
-				SetProfitTarget(CalculationMode.Ticks, TakeProfitTicks); // Set the take profit
-			}
-		}
+                sma200 = SMA(SmaPeriod);
+                AddChartIndicator(sma200);
 
-		protected override void OnBarUpdate()
-		{
-			if (BarsInProgress != 0)
-				return;
+                SetStopLoss(CalculationMode.Ticks, StopLossTicks);
+                SetProfitTarget(CalculationMode.Ticks, TakeProfitTicks);
+            }
+        }
 
-			bool MarketOpen = ToTime(Time[0]) >= 090000 && ToTime(Time[0]) <= 145000;
+        protected override void OnBarUpdate()
+        {
+            if (BarsInProgress != 0)
+                return;
 
-			bool HasCrossedAbove = CrossAbove(macd.Default, macd.Avg, 1);
-			bool HasCrossedBelow = CrossBelow(macd.Default, macd.Avg, 1);
+            bool MarketOpen = ToTime(Time[0]) >= 090000 && ToTime(Time[0]) <= 145000;
 
-			bool PriceAboveSMA = Close[0] > sma200[0];
-			bool PriceBelowSMA = Close[0] < sma200[0];
+            bool HasCrossedAbove = CrossAbove(macd.Default, macd.Avg, 1);
+            bool HasCrossedBelow = CrossBelow(macd.Default, macd.Avg, 1);
 
-			if (MarketOpen)
-			{
-				if (HasCrossedAbove && PriceAboveSMA)
-				{
-					EnterLong(Convert.ToInt32(DefaultQuantity), "");
-				}
-				else if (HasCrossedBelow && PriceBelowSMA)
-				{
-					EnterShort(Convert.ToInt32(DefaultQuantity), "");
-				}
-			}
+            bool PriceAboveSMA = Close[0] > sma200[0];
+            bool PriceBelowSMA = Close[0] < sma200[0];
 
-			if (!MarketOpen)
-			{
-				ExitLong();
-				ExitShort();
-			}
-		}
+            bool cooldownElapsed = Time[0] >= lastPositionExitTime.AddMinutes(CooldownMinutes);
 
-		#region Properties
-		[NinjaScriptProperty]
-		[Range(1, int.MaxValue)]
-		[Display(Name = "FastPeriod", Order = 1, GroupName = "Parameters")]
-		public int FastPeriod { get; set; }
+            if (MarketOpen)
+            {
+                if (cooldownElapsed)
+                {
+					Print("Trading resumed at " + Time[0]);
+                    if (HasCrossedAbove && PriceAboveSMA)
+                    {
+                        EnterLong(Convert.ToInt32(DefaultQuantity), "");
+						Print("Entered long at " + Time[0]);
+                    }
+                    else if (HasCrossedBelow && PriceBelowSMA)
+                    {
+                        EnterShort(Convert.ToInt32(DefaultQuantity), "");
+						Print("Entered short at " + Time[0]);
+                    }
+                }
+                else
+                {
+                    // Log message to check cooldown status
+                    Print("Cooldown period active. Trading will resume at: " + (lastPositionExitTime.AddMinutes(CooldownMinutes)));
+                }
+            }
 
-		[NinjaScriptProperty]
-		[Range(1, int.MaxValue)]
-		[Display(Name = "SlowPeriod", Order = 2, GroupName = "Parameters")]
-		public int SlowPeriod { get; set; }
+            if (!MarketOpen)
+            {
+                ExitLong();
+                ExitShort();
+            }
+        }
 
-		[NinjaScriptProperty]
-		[Range(1, int.MaxValue)]
-		[Display(Name = "SignalPeriod", Order = 3, GroupName = "Parameters")]
-		public int SignalPeriod { get; set; }
+        protected override void OnExecutionUpdate(Execution execution, string executionId, double price, int quantity, MarketPosition marketPosition, string orderId, DateTime time)
+        {
+            if (execution.Order.OrderState == OrderState.Filled)
+            {
+                if (Position.MarketPosition == MarketPosition.Flat)
+                {
+                    Draw.ArrowUp(this, "Exit" + orderId, true, 0, price + 20, Brushes.Yellow);
+                    lastPositionExitTime = Time[0]; // Update the last position exit time to the current bar's time
+                    Print("Updated lastPositionExitTime to: " + lastPositionExitTime);
+                }
+            }
+        }
 
-		[NinjaScriptProperty]
-		[Range(1, int.MaxValue)]
-		[Display(Name = "SmaPeriod", Order = 4, GroupName = "Parameters")]
-		public int SmaPeriod { get; set; }
+        #region Properties
+        [NinjaScriptProperty]
+        [Range(1, int.MaxValue)]
+        [Display(Name = "FastPeriod", Order = 1, GroupName = "Parameters")]
+        public int FastPeriod { get; set; }
 
-		[NinjaScriptProperty]
-		[Range(1, double.MaxValue)]
-		[Display(Name = "Stop Loss (ticks)", Order = 5, GroupName = "Parameters")]
-		public double StopLossTicks { get; set; }
+        [NinjaScriptProperty]
+        [Range(1, int.MaxValue)]
+        [Display(Name = "SlowPeriod", Order = 2, GroupName = "Parameters")]
+        public int SlowPeriod { get; set; }
 
-		[NinjaScriptProperty]
-		[Range(1, double.MaxValue)]
-		[Display(Name = "Take Profit (ticks)", Order = 6, GroupName = "Parameters")]
-		public double TakeProfitTicks { get; set; }
-		#endregion
-	}
+        [NinjaScriptProperty]
+        [Range(1, int.MaxValue)]
+        [Display(Name = "SignalPeriod", Order = 3, GroupName = "Parameters")]
+        public int SignalPeriod { get; set; }
+
+        [NinjaScriptProperty]
+        [Range(1, int.MaxValue)]
+        [Display(Name = "SmaPeriod", Order = 4, GroupName = "Parameters")]
+        public int SmaPeriod { get; set; }
+
+        [NinjaScriptProperty]
+        [Range(1, double.MaxValue)]
+        [Display(Name = "Stop Loss (ticks)", Order = 5, GroupName = "Parameters")]
+        public double StopLossTicks { get; set; }
+
+        [NinjaScriptProperty]
+        [Range(1, double.MaxValue)]
+        [Display(Name = "Take Profit (ticks)", Order = 6, GroupName = "Parameters")]
+        public double TakeProfitTicks { get; set; }
+
+        [NinjaScriptProperty]
+        [Range(1, int.MaxValue)]
+        [Display(Name = "Cooldown (minutes)", Order = 7, GroupName = "Parameters")]
+        public int CooldownMinutes { get; set; }
+        #endregion
+    }
 }
