addb9984886607ea53f90a91a646941efecc03cd
treanor <scyros2000@hotmail.com>
2024-09-01 11:39:16 -0500

    Built out rough code but still needs work

--- SuperTrendUnlocked.cs
+++ SuperTrendUnlocked.cs
@@ -27,59 +27,121 @@ namespace NinjaTrader.NinjaScript.Strategies
 {
 	public class SuperTrendUnlocked : Strategy
 	{
+        private double atrValue;
+        private double superTrendUpper;
+        private double superTrendLower;
+        private double superTrend;
+        private bool isUpTrend;
+        private bool isDownTrend;
+        private Series<double> superTrendSeries;
+
 		protected override void OnStateChange()
 		{
 			if (State == State.SetDefaults)
 			{
-				Description									= @"Enter the description for your new custom Strategy here.";
-				Name										= "SuperTrendUnlocked";
-				Calculate									= Calculate.OnBarClose;
-				EntriesPerDirection							= 1;
-				EntryHandling								= EntryHandling.AllEntries;
-				IsExitOnSessionCloseStrategy				= true;
-				ExitOnSessionCloseSeconds					= 30;
-				IsFillLimitOnTouch							= false;
-				MaximumBarsLookBack							= MaximumBarsLookBack.TwoHundredFiftySix;
-				OrderFillResolution							= OrderFillResolution.Standard;
-				Slippage									= 0;
-				StartBehavior								= StartBehavior.WaitUntilFlat;
-				TimeInForce									= TimeInForce.Gtc;
-				TraceOrders									= false;
-				RealtimeErrorHandling						= RealtimeErrorHandling.StopCancelClose;
-				StopTargetHandling							= StopTargetHandling.PerEntryExecution;
-				BarsRequiredToTrade							= 20;
-				// Disable this property for performance gains in Strategy Analyzer optimizations
-				// See the Help Guide for additional information
-				IsInstantiatedOnEachOptimizationIteration	= true;
-				ATRLength					= 5;
-				ATRFactor					= 3;
+				Description = @"Super Trend strategy based on ATR.";
+				Name = "SuperTrendUnlocked";
+				Calculate = Calculate.OnBarClose;
+				EntriesPerDirection = 1;
+				EntryHandling = EntryHandling.AllEntries;
+				IsExitOnSessionCloseStrategy = true;
+				ExitOnSessionCloseSeconds = 30;
+				IsFillLimitOnTouch = false;
+				MaximumBarsLookBack = MaximumBarsLookBack.TwoHundredFiftySix;
+				OrderFillResolution = OrderFillResolution.Standard;
+				Slippage = 1;
+				StartBehavior = StartBehavior.WaitUntilFlat;
+				TimeInForce = TimeInForce.Gtc;
+				TraceOrders = false;
+				RealtimeErrorHandling = RealtimeErrorHandling.StopCancelClose;
+				StopTargetHandling = StopTargetHandling.PerEntryExecution;
+				BarsRequiredToTrade = 20;
+				IsInstantiatedOnEachOptimizationIteration = true;
+				ATRLength = 5;
+				ATRFactor = 3;
 			}
 			else if (State == State.Configure)
+			{
+				// AddDataSeries(Data.BarsPeriodType, 1); // 1-minute bars for calculations
+				superTrendSeries = new Series<double>(this);
+			}
+			else if (State == State.DataLoaded)
 			{
 			}
 		}
 
 		protected override void OnBarUpdate()
 		{
-			if (BarsInProgress != 0) 
+			if (CurrentBar < ATRLength)
 				return;
 
+			// Calculate ATR
+			atrValue = ATR(ATRLength)[0];
+
+			// Calculate SuperTrend levels
+			superTrendUpper = High[0] - ATRFactor * atrValue;
+			superTrendLower = Low[0] + ATRFactor * atrValue;
+
+			// Determine trend
+			if (Close[0] > superTrendUpper)
+			{
+				if (superTrendLower > superTrendSeries[1])
+					superTrend = superTrendLower;
+				else
+					superTrend = superTrendSeries[1];
+				// superTrend = superTrendLower;
+				isUpTrend = true;
+				isDownTrend = false;
+			}
+			else if (Close[0] < superTrendLower)
+			{
+				if (superTrendUpper < superTrendSeries[1])
+					superTrend = superTrendLower;
+				else
+					superTrend = superTrendSeries[1];
+				// superTrend = superTrendUpper;
+				isUpTrend = false;
+				isDownTrend = true;
+			}
+
+			Print("SuperTrend: " + superTrend);
+			Print("Last SuperTrend: " + superTrendSeries[1]);
+			Print("Close: " + Close[0]);
+			Print("isUpTrend: " + isUpTrend);
+			Print("isDownTrend: " + isDownTrend);
 			
+
+			// Store SuperTrend value
+			superTrendSeries[0] = superTrend;
+
+			// Plotting SuperTrend
+			PlotSuperTrend();
+		}
+
+		private void PlotSuperTrend()
+		{
+			// Plot uptrend in green
+			if (isUpTrend)
+			{
+				Draw.Line(this, "UpTrend_" + CurrentBar, false, 1, superTrendSeries[1], 0, superTrend, Brushes.Green, DashStyleHelper.Solid, 2);
+			}
+			// Plot downtrend in red
+			else if (isDownTrend)
+			{
+				Draw.Line(this, "DownTrend_" + CurrentBar, false, 1, superTrendSeries[1], 0, superTrend, Brushes.Red, DashStyleHelper.Solid, 2);
+			}
 		}
 
 		#region Properties
 		[NinjaScriptProperty]
 		[Range(1, int.MaxValue)]
-		[Display(Name="ATRLength", Order=1, GroupName="Parameters")]
-		public int ATRLength
-		{ get; set; }
+		[Display(Name = "ATR Length", Order = 1, GroupName = "Parameters")]
+		public int ATRLength { get; set; }
 
 		[NinjaScriptProperty]
-		[Range(1, int.MaxValue)]
-		[Display(Name="ATRFactor", Order=2, GroupName="Parameters")]
-		public int ATRFactor
-		{ get; set; }
+		[Range(0.01, double.MaxValue)]
+		[Display(Name = "ATR Factor", Order = 2, GroupName = "Parameters")]
+		public double ATRFactor { get; set; }
 		#endregion
-
 	}
 }
